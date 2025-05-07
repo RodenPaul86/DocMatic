bba0e126461776b70838c4b601be91b6329151e5
Paul Roden Jr <per312@icloud.com>
2025-05-06 19:39:58 -0500

    Fixed a bug that removed my apiKey

---
+++ DocMatic/Subscripion Services/apiKeys.swift
@@ -0,0 +1,12 @@
+//
+//  apiKeys.swift
+//  TaskSync
+//
+//  Created by Paul  on 4/12/25.
+//
+
+import Foundation
+
+struct apiKeys {
+    static let revenueCat: String = "appl_VOFPplPzOlqpLpeFdFTVgePvUuB"
+}
