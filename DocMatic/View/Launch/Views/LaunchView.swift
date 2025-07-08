b12bd50a88b4a8ddf17eb3828985b575e8687656
Paul Roden Jr <per312@icloud.com>
2025-07-08 10:56:40 -0500

    Fixed the loading title.

--- DocMatic/View/Launch/Views/LaunchView.swift
+++ DocMatic/View/Launch/Views/LaunchView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct LaunchView: View {
-    @State private var loadingText: [String] = "Loading...".map { String($0) }
+    @State private var loadingText: [String] = "Loading content...".map { String($0) }
     @State private var showLoadingText: Bool = false
     private let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
     
